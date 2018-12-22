4ae57c13907fd78aa1bba880577bae3eeee5ab94
rajesh23-drg <rmanjunath@teamdrg.com>
2018-12-22 16:36:09 +0530

    Intial Commit

---
+++ src/main/java/com/rajesh/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.rajesh.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+	
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+}
